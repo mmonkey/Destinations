872258a0c68e937da30f4fe568d382baf1386deb
mmonkey <mmonkey@gmail.com>
2015-04-30 00:16:59 -0500

    Updated version number

--- src/main/java/com/github/mmonkey/Destinations/Destinations.java
+++ src/main/java/com/github/mmonkey/Destinations/Destinations.java
@@ -28,7 +28,7 @@ public class Destinations {
 	
 	public static final String NAME = "Destinations";
 	public static final String ID = "Destinations";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.0.1";
 	
 	private Game game;
 	private Optional<PluginContainer> pluginContainer;
