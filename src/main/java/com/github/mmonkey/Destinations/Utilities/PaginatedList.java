777446227900c332c7dbdc4a6a5b47164ab75e63
mmonkey <mmonkey@gmail.com>
2015-05-02 03:08:22 -0500

    Fixed last item in PaginatedList not showing bug.

--- src/main/java/com/github/mmonkey/Destinations/Utilities/PaginatedList.java
+++ src/main/java/com/github/mmonkey/Destinations/Utilities/PaginatedList.java
@@ -80,8 +80,8 @@ public class PaginatedList {
 	}
 	
 	public List<Text> subList(int fromIndex, int toIndex) {
-		int from = ((this.size() - 1) > fromIndex) ? fromIndex : (this.size() - 1);
-		int to = ((this.size() - 1) > toIndex) ? toIndex : (this.size() - 1);
+		int from = (this.size() > fromIndex) ? fromIndex : this.size();
+		int to = (this.size() > toIndex) ? toIndex : this.size();
 		return this.items.subList(from, to);
 	}
 	
