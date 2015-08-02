94842b2a002bb0d44f4614c1fb8ebe3bc8878bf9
mmonkey <mmonkey@gmail.com>
2015-08-01 20:01:34 -0500

    fixed updating homes

--- src/main/java/com/github/mmonkey/Destinations/Dams/HomeDam.java
+++ src/main/java/com/github/mmonkey/Destinations/Dams/HomeDam.java
@@ -7,8 +7,6 @@ import com.github.mmonkey.Destinations.Models.HomeModel;
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.world.World;
 
-import javax.print.attribute.standard.Destination;
-import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -96,73 +94,14 @@ public class HomeDam {
 
     public HomeModel getPlayerHomeByName(Player player, String name) {
 
-        int playerId = this.playerDam.getPlayerId(player.getUniqueId());
-
-        HomeModel home = null;
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet result = null;
-        String sql = "SELECT" +
-                " homes.id," +
-                " homes.name," +
-                " homes.owner_id," +
-                " destinations.id," +
-                " destinations.x," +
-                " destinations.y," +
-                " destinations.z," +
-                " destinations.yaw," +
-                " destinations.pitch," +
-                " destinations.roll," +
-                " worlds.unique_id" +
-                " FROM " + tblName +
-                " JOIN destinations ON homes.destination_id = destinations.id" +
-                " JOIN worlds ON destinations.world_id = worlds.id" +
-                " JOIN players ON homes.owner_id = players.id" +
-                " WHERE homes.owner_id = ?" +
-                " AND UPPER(homes.name) = UPPER(?)" +
-                " LIMIT 1";
-
-        try {
-
-            connection = database.getConnection();
-            statement = connection.prepareStatement(sql);
-            statement.setInt(1, playerId);
-            statement.setString(2, name);
-            result = statement.executeQuery();
-
-            while (result.next()) {
-
-                DestinationModel destination = new DestinationModel(
-                        result.getInt("destinations.id"),
-                        UUID.fromString(result.getString("worlds.unique_id")),
-                        result.getDouble("destinations.x"),
-                        result.getDouble("destinations.y"),
-                        result.getDouble("destinations.z"),
-                        result.getDouble("destinations.yaw"),
-                        result.getDouble("destinations.pitch"),
-                        result.getDouble("destinations.roll")
-                );
-
-                home = new HomeModel(
-                        result.getInt("homes.id"),
-                        result.getString("homes.name"),
-                        destination
-                );
+        ArrayList<HomeModel> homes = this.getPlayerHomes(player);
+        for (HomeModel home : homes) {
+            if (home.getName().equalsIgnoreCase(name)) {
+                return home;
             }
-
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-
-        } finally {
-
-            try { if (result != null) result.close(); } catch (SQLException e) { e.printStackTrace(); }
-            try { if (statement != null) statement.close(); } catch (SQLException e) { e.printStackTrace(); }
-            try { if (connection != null) connection.close(); } catch (SQLException e) { e.printStackTrace(); }
-
         }
 
-        return this.isHomeInGameWorlds(home) ? home : null;
+        return null;
     }
 
     public HomeModel insertHome(Player player, String name) {
@@ -250,41 +189,6 @@ public class HomeDam {
         return home;
     }
 
-    public HomeModel updateHome(HomeModel home, String newName) {
-
-        Connection connection = null;
-        PreparedStatement statement = null;
-
-        int updatedRows = 0;
-
-        String sql = "UPDATE " + tblName + " SET name = ? WHERE id = ?";
-
-        try {
-
-            connection = database.getConnection();
-            statement = connection.prepareStatement(sql);
-            statement.setString(1, newName);
-            statement.setInt(2, home.getId());
-            updatedRows = statement.executeUpdate();
-
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-
-        } finally {
-
-            try { if (statement != null) statement.close(); } catch (SQLException e) { e.printStackTrace(); }
-            try { if (connection != null) connection.close(); } catch (SQLException e) { e.printStackTrace(); }
-
-        }
-
-        if (updatedRows > 0) {
-            home.setName(newName);
-        }
-
-        return home;
-    }
-
     public boolean deleteHome(Player player, HomeModel home) {
 
         int playerId = this.playerDam.getPlayerId(player.getUniqueId());
@@ -340,23 +244,6 @@ public class HomeDam {
         }
 
         return filteredHomes;
-    }
-
-    private boolean isHomeInGameWorlds(HomeModel home) {
-
-        if (home == null) {
-            return false;
-        }
-
-        Collection<World> worlds = plugin.getGame().getServer().getWorlds();
-
-        for (World world : worlds) {
-            if (home.getDestination().getWorldUniqueId().equals(world.getUniqueId())) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     public HomeDam(Destinations plugin) {
